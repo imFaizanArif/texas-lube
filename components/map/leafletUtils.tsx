3712f0bb109f8c0152e83089345f8e3425a880e8
imFaizanArif <faizanarif1245@gmail.com>
2024-11-26 02:34:19 +0500

    initial commit

---
+++ components/map/leafletUtils.tsx
@@ -0,0 +1,14 @@
+import L from "leaflet";
+
+export const createMarkerIcon = () => {
+    if (typeof window === "undefined") return null;
+
+    return new L.Icon({
+        iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+        shadowSize: [41, 41],
+    });
+};
